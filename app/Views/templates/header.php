d9b64625a38d2e11f9e224171538bb06a28da373
Clement-GRASSET <clement.grasset@etu.univ-amu.fr>
2021-11-23 10:46:48 +0100

    Début de la page d'accueil

---
+++ app/Views/templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>PROJEEEEEEEEEEEEEEET</title>
+    <link rel="stylesheet" href="/styles/normalize.css">
+    <link rel="stylesheet" href="/styles/styles.css">
+</head>
+
+<body>
+
+    <header>
+        <p>Se connecter</p>
+    </header>
