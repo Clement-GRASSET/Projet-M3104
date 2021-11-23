fd310d686a0822671af136f342c1bf3795043d32
Clement-GRASSET <clement.grasset@etu.univ-amu.fr>
2021-11-23 09:09:16 +0100

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,5 +1,5 @@
 create table T_annonce (
-    A_idannonce serial,
+    A_idannonce serial primary key,
     A_titre varchar(255),
     A_cout_loyer decimal(14, 2),
     A_cout_charges decimal(14, 2),
@@ -13,21 +13,31 @@ create table T_annonce (
 );
 
 create table T_energie (
-
+    E_id_engie serial primary key,
+    E_description varchar(255)
 );
 
 create table T_utilisateur (
-
+    U_mail varchar(255) primary key,
+    U_mdp char(64),
+    U_pseudo varchar(255),
+    U_nom varchar(255),
+    U_prenom varchar(255),
+    U_admin boolean
 );
 
 create table T_message (
-
+    M_dateheure_message time,
+    M_texte_message varchar(255)
 );
 
 create table T_photo (
-
+    P_id_photo serial primary key,
+    P_titre varchar(255),
+    P_nom varchar(255)
 );
 
 create table T_typeMaison (
-
+    T_type char(2) primary key,
+    T_description varchar(255)
 );
